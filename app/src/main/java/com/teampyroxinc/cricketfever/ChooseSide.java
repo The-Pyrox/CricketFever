ec0589dd7d0ea2286b1b03e450830333b53d5760
poonnu <poojanrathod@gmail.com>
2016-08-18 15:01:45 +0530

    Initial commit

---
+++ app/src/main/java/com/teampyroxinc/cricketfever/ChooseSide.java
@@ -0,0 +1,30 @@
+package com.teampyroxinc.cricketfever;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ChooseSide extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_choose_side);
+    }
+
+    public void bat_click(View view){
+        Intent i = new Intent(this,Batting.class);
+        startActivity(i);
+    }
+
+    public void bowl_click(View view){
+        Intent j = new Intent(this,Bowling.class);
+        startActivity(j);
+    }
+}
